33125c483c287d62370d72aa9d879e1ffaf14ed2
dowoon <woodw920@gmail.com>
2022-10-28 13:39:14 +0900

    #3 - 플러시 flush()

--- src/main/java/com/example/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/com/example/ex1hellojpa/hellojpa/JpaMain.java
@@ -16,14 +16,12 @@ public class JpaMain {
 
         try {
             // 영속
-            // 변경 감지
-            // 엔티티 수정은 변경감지에 의해서 하게된다.
-            Member member = em.find(Member.class, 100L);
-            member.setName("ㅋㅋㅋ"); // em.persist 를 해주지 않아도 변경된다. -> 자바컬렉션을 이용하는 것 처럼 이용하면 되는 부분이다.
+            Member member = new Member();
+            member.setId(200L);
+            member.setName("member");
+            em.persist(member);
 
-            // 삭제
-            Member memberA = em.find(Member.class, 100L);
-            em.remove(memberA);
+            em.flush(); // 플러시. 이시점에 데이터베이스 쿼리가 바로 날라가게 된다.
 
             tx.commit(); // 트랜잭션 커밋 -> 저장
         } catch (Exception e) {
