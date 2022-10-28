c6c0f374275ee5e6d9a890e9b14ba011eab85567
dowoon <woodw920@gmail.com>
2022-10-28 15:28:52 +0900

    #4 - 필드와 컬럼 매핑

--- src/main/java/com/example/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/com/example/ex1hellojpa/hellojpa/JpaMain.java
@@ -15,18 +15,7 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-            // 영속
-            Member member = em.find(Member.class, 100L);
-            member.setName("AAAA");
-
-            // 준영속상태 디태치
-            em.detach(member); // 더이상 JPA에서 관리하지않는다. 쿼리문 실행 XX
-
-            em.clear(); // 모든걸 초기화, 준영속상태로 변경, 디태치 상태
-
-            em.close(); // 영속성 컨텍스트를 종료
-
-            tx.commit(); // 트랜잭션 커밋 -> 저장
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/com/example/ex1hellojpa/hellojpa/Member.java
+++ src/main/java/com/example/ex1hellojpa/hellojpa/Member.java
@@ -1,38 +1,39 @@
 package com.example.ex1hellojpa.hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity // JPA를 사용하는 애구나 하고 알게된다. 애노테이션
 // @Table(name = "User") 테이블 이름이 다를 경우 이런식으로 매핑할 수 있다.
 public class Member {
-
     @Id
     private Long id;
-    // @Column(name = "username") 컬럼명도 다를 경우 이런식으로 매핑이 가능하다.
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) // enum 타입을 사용할 수 있다.
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate; // 년 월
+    private LocalDateTime testLocalDateTime; // 년 월 일
+
+    @Lob
+    private String description;
+
+    @Transient // 난 DB에 얠 넣고싶지않아 신경쓰고싶지않아, 할때 쓰는것.
+    private int temp;
 
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
