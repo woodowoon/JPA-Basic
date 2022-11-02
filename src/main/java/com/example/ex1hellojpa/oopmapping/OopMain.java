ee40171696e3222ce980f0dfa1aa66b4793dbc52
dowoon <woodw920@gmail.com>
2022-11-02 17:35:29 +0900

    #14 - 값타입 컬렉션 조회
    값 타입 컬렉은 Lazy, 디폴트가 지연로딩이라는 것을 알 수 있다.

--- src/main/java/com/example/ex1hellojpa/oopmapping/OopMain.java
+++ src/main/java/com/example/ex1hellojpa/oopmapping/OopMain.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
 
 public class OopMain {
     public static void main(String[] args) {
@@ -17,46 +19,43 @@ public class OopMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         /**
-         * - 엔티티타입
-         *      - 데이터가 변해도 식별자로 지속해서 추적 가능하다.
-         *      - @Entity 로 정의하는 객체
-         * - 값타입
-         *      - 식별자가 없고 값만 있으므로, 변경시 추적 불가능하다.
-         *      - int, Integer, String 처럼 단순히 값으로 사용하는 자바 기본 타입이나 객체
-         *      - 생명주기를 엔티티에 의존한다.
-         *      - ex) 회원을 삭제하는 순간 이름과 나이 필드도 함께 삭제된다.
-         *      - 값 타입은 공유하면 안된다.
-         *      - ex) 회원 이름 변경시 다른 회원의 이름도 함께 변경되면 안된다.
-         * - 임베디드 타입(embedded type, 복합 값 타입)
-         *      - int, string 같은 타입이다.
-         *      - ex) 회원 엔티티는 이름, 근무시작일, 근무종료일, 주소도시, 주소번지, 주소우편번호를 갖는다.
-         *          - 회원 엔티티는 이름, 근무기간, 집주소를 가진다. 라고 설명할 수 있다.
-         *          - @Embeddable : 값 타입을 정의하는 곳에 표시
-         *          - @Embedded : 값 타입을 사용하는 곳에 표시
-         *      - 장점 : 높은 재사용, 높은 응집도, 해당 값 타입만 사용하는 의미있는 메소드를 만들 수 있다., 엔티티 생명주기에 의존한다.
-         * - 컬렉션 값 타입(collection value type)
+         * - 값 타입의 비교
+         *  - 값 타입: 인스턴스가 달라도 그 안에 값이 같으면 같은 것으로 봐야한다.
+         *
+         * - 값 타입 컬렉션
+         *  - 값타입을 컬렉션에 담아서 쓰는것.
          */
         try {
-            Address address = new Address("city", "street", "10");
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setHomeaddress(address);
-            em.persist(member1);
+            Member member = new Member();
+            member.setUsername("member");
+            member.setHomeAddress(new Address("homecity", "street", "1000"));
 
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode()); // 복사해서 사용해야한다.
+            member.getFavoriteFoods().add("피자");
+            member.getFavoriteFoods().add("소고기");
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            // member2.setHomeaddress(address);  // 이렇게 사용하면 안된다.
-            member2.setHomeaddress(copyAddress);
-            em.persist(member2);
+            member.getAddressesHistory().add(new Address("old1", "street", "1000"));
+            member.getAddressesHistory().add(new Address("old2", "street", "1000"));
 
-            // 나는 member1 의 주소만 newCity 로 바꾸고 싶어.
-            // 근데 member1과 member2 둘다 바뀌어버린다.
-            // 값타입을 공유하면 굉장히 위험하다.
-            // 대신 값을 복사해서 사용해야한다.
-            member1.getHomeaddress().setCity("newCity");
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("=============== START =================");
+            Member findMember = em.find(Member.class, member.getId());
+            System.out.println("===============  END =================");
+
+            // 컬렉션은 Lazy : 지연로딩이라는 뜻이다.
+            List<Address> addressHistory = findMember.getAddressesHistory();
+            for(Address address : addressHistory) {
+                System.out.println("address = " + address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for(String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/com/example/ex1hellojpa/oopmapping/domain/Member.java
+++ src/main/java/com/example/ex1hellojpa/oopmapping/domain/Member.java
@@ -1,6 +1,10 @@
 package com.example.ex1hellojpa.oopmapping.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -12,16 +16,48 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    // 집 주소
     @Embedded
-    private Address Homeaddress;
+    private Address homeAddress;
 
-    // 기간 Period
-    @Embedded
-    private Period workPeriod;
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressesHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressesHistory() {
+        return addressesHistory;
+    }
+
+    public void setAddressesHistory(List<Address> addressesHistory) {
+        this.addressesHistory = addressesHistory;
     }
 
     public void setId(Long id) {
@@ -36,19 +72,4 @@ public class Member {
         this.username = username;
     }
 
-    public Address getHomeaddress() {
-        return Homeaddress;
-    }
-
-    public void setHomeaddress(Address homeaddress) {
-        Homeaddress = homeaddress;
-    }
-
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
 }
