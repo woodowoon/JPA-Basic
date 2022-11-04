84c0d37ed3d06e7168fbb686e6c2e0ede361ab62
dowoon <woodw920@gmail.com>
2022-11-04 13:18:15 +0900

    #15 끝

--- src/main/java/com/example/ex1hellojpa/hellojpa/JpaMain.java
+++ src/main/java/com/example/ex1hellojpa/hellojpa/JpaMain.java
@@ -6,6 +6,12 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.List;
 
+/**
+ * JPQL
+ * - JPQL은 객제지향 쿼리 언어다. 따라서 테이블을 대상으로 쿼리하는 것이 아니라 엔티티 객체를 대상으로 쿼리한다.
+ * - JPQL은 SQL을 추상화 해서 특정 데이터베이스 SQL에 의존하지 않는다.
+ * - JPQL은 결국 SQL로 변환된다.
+ */
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -14,7 +20,6 @@ public class JpaMain {
 
         EntityTransaction tx = em.getTransaction(); // 트랜잭션
         tx.begin(); // 트랜잭션 시작
-
         try {
             List<Member> result = em.createQuery(
                     "select m from Member m where m.username like  '%kim%'",
